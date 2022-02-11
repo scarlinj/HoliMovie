31f20f27589c5e6ce15d158d7351da257b192d5f
jeongholee <jeongholee@hotmail.com>
2022-02-11 11:09:47 -0800

    add movie and tvshow API

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,130 +1,122 @@
-var repoContainerEl = document.querySelector("#repos-container");
-var repoSearchTerm = document.querySelector("#repo-search-term");
-var getUserRepos = function (user) {
-    // format the github api url
-    // var apiUrl = "https://api.spotify.com/v1";
+// Public Holiday API (1922 ~ 2122) 200years
+const yearInput = document.querySelector(`#yearInput`);
+const submitBtn = document.querySelector(`#submitSearch`);
+const holidayContainer = document.querySelector(`#holidayContainer`)
 
-    // make a request to the url
-    // to use fetch():
-    fetch(apiUrl).then(function (response) {
-            return response.json();
-        })
-        .then(function (data) {
-            console.log(data);
-            displayRepos(data, user);
+submitBtn.addEventListener(`click`, function(event) {
+event.preventDefault();
+var year = yearInput.value;
+var country = countryCode.value;
+var searchUrl = `https://date.nager.at/api/v3/PublicHolidays/${year}/${country}`;
 
-
-        });
-};
-
-var getInfo = function () {
-    fetch("https://date.nager.at/api/v3/AvailableCountries").then(function (response) {
+    fetch(searchUrl)
+    .then(function (response) {
         response.json().then(function (data) {
+        
             console.log(data);
-            console.log(data[17].name); // example for Canada
-            console.log(data[73].name); // example for Mexico
-            console.log(data[103].name); // example for USA
+
+            for (let i = 0; i < data.length; i++) {
+
+                const {localName, date, countryCode, type} = data[i];
+                console.log(localName, date, countryCode, type);
+                
+                var text = document.createElement(`h1`);
+                
+                holidayContainer.appendChild(text);
+            }
         });
     });
-}
-getInfo();
+});
 
-// from Ryan:
-// fetch(apiUrl).then(function (response) {
-//         return response.json();
-//     })
-//     .then(function (data) {
-//         console.log(data);
-//         displayRepos(data, user);
-//     });
+// Movie Search
 
-// Hotels try (this has a CORS function)
-// const output = document.querySelector('.output');
-// fetch("https://hotels-com-provider.p.rapidapi.com/v1/destinations/search?query=denver&currency=USD&locale=en_US", {
-//         "method": "GET",
-//         "headers": {
-//             "x-rapidapi-host": "hotels-com-provider.p.rapidapi.com",
-//             "x-rapidapi-key": "6f3dbc74a2msh4c78db503bca4f9p161b7ejsn0002ad20a68c"
-//         }
-//     })
-//     .then(response => {
-//         console.log(response);
-//         return response.json();
-//     }).then(function (data) {
-//         console.log(JSON.stringify(data));
-//         const destinations = Object.entries(data);
-//         console.log(destinations);
-//         for (const [key, value] of Object.entries(destinations)) {
-//             console.log(`${key}; ${value}`);
-//         }
-//     });
+var apiKey = `6bbce96a`;
 
-var userFormEl = document.querySelector("#user-form");
+$(document).ready(function(){
+    $(`#moviesearch`).submit(function(event){
+        event.preventDefault()
 
-var nameInputEl = document.querySelector("#username");
+        var movie = $(`#movies`).val()
+        var result = ""
+        var searchUrl = `http://www.omdbapi.com/?t=` + movie + `&apikey=` + apiKey;
+        
+        $.ajax({
+            method:`GET`,
+            url: searchUrl,
+            success: function(data){
+                
+                console.log(data);
 
-var formSubmitHandler = function (event) {
-    event.preventDefault();
-    // get value from input element
-    var username = nameInputEl.value.trim();
+                result = `<img src="${data.Poster}"/>
+                <h2>${data.Title}<h2>
+                `;
 
-    if (username) {
-        getUserRepos(username);
-        nameInputEl.value = "";
-    } else {
-        alert("Please enter a GitHub username");
-    }
-    console.log(event);
-};
+                $("#movieContainer").html(result);
+            }
+        })
+    })
+})
 
-var displayRepos = function (repos, searchTerm) {
-    //clear old content
-    repoContainerEl.textContent = "";
-    repoSearchTerm.textContent = searchTerm;
-    // var repoContainerEl = document.querySelector("#repos-container");
-    // var repoSearchTerm = document.querySelector("#repo-search-term");
+// TV Search
 
-    console.log(repos);
-    console.log(searchTerm);
+$(document).ready(function(){
+    $(`#tvsearch`).submit(function(event){
+        event.preventDefault()
 
-    // loop over repos
-    for (var i = 0; i < repos.length; i++) {
-        // format repo name
-        var repoName = repos[i].owner.login + "/" + repos[i].name;
+        var tvs = $(`#tvs`).val()
+        var results = ""
+        var searchUrl = `http://api.tvmaze.com/search/shows?q=${tvs}`;
+        
+        $.ajax({
+            method:`GET`,
+            url: searchUrl,
+            success: function(data){
+                
+                console.log(data[0].show.image);
+                console.log(data[0].show.name);
 
-        // create a container for each repo
-        var repoEl = document.createElement("div");
-        repoEl.classList = "list-item flex-row justify-space-between align-center";
+                results = `<img src="${data[0].show.image.medium}"/>
+                <h2>${data[0].show.name}<h2>
+                `;
 
-        // create a span element to hold repository name
-        var titleEl = document.createElement("span");
-        titleEl.textContent = repoName;
-
-        // append to container
-        repoEl.appendChild(titleEl);
-
-        // create a status element
-        var statusEl = document.createElement("span");
-        statusEl.classList = "flex-row align-center";
-
-        // check if current repo has issues or not
-        if (repos[i].open_issues_count > 0) {
-            statusEl.innerHTML =
-                "<i class='fas fa-times status-icon icon-danger'></i>" + repos[i].open_issues_count + " issue(s)";
-        } else {
-            statusEl.innerHTML = "<i class='fas fa-check-square status-icon icon-success'></i>";
-        }
-
-        // append to container
-        repoEl.appendChild(statusEl);
-
-        // append container to the dom
-        repoContainerEl.appendChild(repoEl);
-    }
-};
-
-userFormEl.addEventListener("submit", formSubmitHandler);
+                $("#tvContainer").html(results);
+            }
+        })
+    })
+})
 
 
+// const movieInput = document.querySelector(`#movies`);
+// const movieBtn = document.querySelector(`#movieSearch`);
+// const movieContainer = document.querySelector(`.movieContainer`)
 
-getUserRepos();
+// movieBtn.addEventListener(`click`, function(event) {
+// event.preventDefault();
+// var keyword = movies.value;
+// var apiKey = `6bbce96a`;
+
+// var searchUrl = `http://www.omdbapi.com/?s=${keyword}&apikey=${apiKey}`;
+
+//     fetch(searchUrl)
+//     .then(function (response) {
+//         //console.log(response);
+//         return response.json()
+//     })    
+//     .then(function (movieResponse) {
+            
+//             console.log(movieResponse);
+
+//             for (var j=0; j < movieResponse.length; j++) {
+//                 var currentMovie = movieResponse[j];
+
+//                 console.log(currentMovie.Poster);
+
+//                 var img = document.createElement(`img`);
+//                 img.setAttribute(`src`,currentMovie.Poster);
+//                 movieContainer.appendChild(img);
+
+            
+//             }
+
+//             });
+//         });
