648643618b258327acbbfdd6e35a12c73a6527fb
Aaron Stokes <stokescomp01@gmail.com>
2022-02-14 15:12:09 -0700

    add refactored html and javascript

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -3,13 +3,12 @@ const submitBtn = document.querySelector(`#submitSearch`);
 const countryCode = document.querySelector(`#countryCode`);
 const holidayContainer = document.querySelector(`#holidayContainer`)
 const movieContainer = document.querySelector(`#movieContainer`)
-const myInput = document.querySelector('#dropdown');
-let dropdownValue = ``;
 
 // Country Code Drop Down
+let dropdownValue = ``;
 let dropdown = document.getElementById('locality-dropdown');
-dropdown.length = 0;
 let defaultOption = document.createElement('option');
+dropdown.length = 0;
 defaultOption.text = '-Countries-';
 dropdown.add(defaultOption);
 dropdown.selectedIndex = 0;
@@ -27,68 +26,44 @@ fetch('https://date.nager.at/api/v3/AvailableCountries')
             dropdown.add(option);
         }
     });
-// Country Code to 
+
+// Country Code
 $("#locality-dropdown").on("change", function (e) {
     dropdownValue = e.target.value;
-    console.log(e.target.value);
 })
 
 // Public Holiday API Using fetch (1922 ~ 2122) 200 years
-// let holidayDropdown = document.getElementById('holiday-dropdown');
-// holidayDropdown.length = 0;
-// let defaultHoliday = document.createElement('option');
-// defaultHoliday.text = 'Finding Holiday';
-// holidayDropdown.add(defaultOption);
-// holidayDropdown.selectedIndex = 0;
 
 submitBtn.addEventListener(`click`, function (event) {
 
     event.preventDefault();
 
     var year = yearInput.value;
-    console.log(year);
     var searchUrl = `https://date.nager.at/api/v3/PublicHolidays/${year}/${dropdownValue}`;
 
     fetch(searchUrl)
         .then(function (response) {
             if (response.ok) {
-
                 return response.json();
             }
-
-
         })
         .then(function (data) {
-            // if (!data) {
-            //     // call handleModal instead of the below
-            //     // var modbtn = document.getElementById("clearModal");
-            //     // $(modbtn).toggle();
-            //     handleModal();
-            //     return;
-            // }
-            holidayContainer.textContent = "";
+            holidayContainerResult.textContent = "";
             for (let j = 0; j < data.length; j++) {
                 const currentData = data[j];
                 console.log(currentData);
                 var p1 = document.createElement(`p`);
-                p1.textContent = currentData.date + ` ` + currentData.localName + ` ` + currentData.countryCode;
+                p1.textContent = "     " + currentData.date + ` ` + currentData.localName + ` ` + currentData.countryCode;
                 holidayContainer.appendChild(p1);
             }
-            // $("#results").empty().append(p1);
             localStorage.setItem("data", JSON.stringify(data));
-            console.log(holidayContainer);
         })
         .catch(function (error) {
             handleModal();
             console.log(error)
         });
 
-    // 'user-form'.textContent = "";
-
-
     // Get the modal *** --FIX THIS --
-    // if (    var modal = document.getElementById("submitSearch");
-    // )
 
     function handleModal() {
         // Get the button that opens the modal
@@ -96,8 +71,7 @@ submitBtn.addEventListener(`click`, function (event) {
         console.log(modal);
         // Get the <span> element that closes the modal
         var span = document.querySelector(".clear");
-
-        // When the user clicks the button, open the modal 
+        // When the user clicks the button, open the modal
         // btn.onclick = function () {
         modal.style.display = "block";
         // }
@@ -116,22 +90,7 @@ submitBtn.addEventListener(`click`, function (event) {
     }
 });
 
-console.log("Hello! Press the button to clear the console!");
-
-function clearHolidays() {
-    holidayContainer.textContent = "";
-
-    // console.clear();
-    // localStorage.clear();
-}
-
-// Country Code to Holiday
-$("#holiday-dropdown").on("change", function (e) {
-    dropdownValue = e.target.value;
-    console.log(e.target.value);
-})
-
-// Movie Search Using ajax
+// Movie Search
 var apiKey = `6bbce96a`;
 $(document).ready(function () {
     $(`#moviesearch`).submit(function (event) {
@@ -146,28 +105,22 @@ $(document).ready(function () {
                 console.log(data);
                 result = `
                 <img src="${data.Poster}"/>
+                <br>
+                <h2>Movie Title<h2>
                 <h2>${data.Title}<h2>
+                <h2>Movie Plot<h2>
                 <h3>${data.Plot}<h3>
                 `;
 
-                $("#movieContainer").html(result);
+                $("#movieContainerResult").html(result);
                 // store in local storage
                 localStorage.setItem("data", JSON.stringify(data));
             }
         })
-        // 'movie-form'.textContent = "";
-        movieContainer.textContent = "";
+
     })
 })
 
 var saveHoliday = function () {
     localStorage.setItem("")
-}
-
-console.log("Hello! Press the button to clear the console!");
-
-function clearMovie() {
-    movieContainer.textContent = "";
-    // console.clear();
-    // localStorage.clear();
 }
